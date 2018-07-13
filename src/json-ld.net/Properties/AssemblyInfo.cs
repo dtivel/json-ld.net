b2cca9b6c096f90bd1bc53687444a5339d0caceb
Damon Tivel <dtivel@microsoft.com>
2018-07-13 13:08:21 -0700

    Bump version

--- src/json-ld.net/Properties/AssemblyInfo.cs
+++ src/json-ld.net/Properties/AssemblyInfo.cs
@@ -9,6 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JsonLD")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyKeyFileAttribute("PublicKeySha256.snk")]
+[assembly: AssemblyDelaySignAttribute(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
